944c4c5f2e5932c4470cefe9870e7b902b9d25b9
Danny Lewis <nitomatik@gmail.com>
2015-10-19 00:30:32 -0600

    Almost forgot about the search bar and back button!

--- RadBox_start/RadBox_start/Pages/PicturesPage.xaml.cs
+++ RadBox_start/RadBox_start/Pages/PicturesPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RadBox_start.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,11 @@ namespace RadBox_start
         public PicturesPage()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Navigator.LandingPage(); 
         }
     }
 }
